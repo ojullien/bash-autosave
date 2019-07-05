0c66a041169e76ded1b1a42a017fa259f3fcf27d
ojullien <ojullien@users.noreply.github.com>
2019-07-05 22:00:40 +0200

    v3.2.0

--- src/app/autosavedb/app.sh
+++
@@ -1,9 +0,0 @@
-## -----------------------------------------------------------------------------
-## Linux Scripts.
-## SaveDB app functions
-##
-## @package ojullien\Shell\app\savedb
-## @license MIT <https://github.com/ojullien/Shell/blob/master/LICENSE>
-## -----------------------------------------------------------------------------
-
-# empty file
